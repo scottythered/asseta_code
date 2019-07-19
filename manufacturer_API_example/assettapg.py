abdf25e2ecae9c490f9d70e4a8475d1399a3f34b
Scotty Carlson <scottythered@users.noreply.github.com>
2019-07-18 23:42:37 -0700

    Add files via upload

---
+++ manufacturer_API_example/assettapg.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+
+def selector(cursor, command, table):
+    if command == 'select_all':
+        cursor.execute("SELECT * FROM {0};".format(table))
+        results = cursor.fetchall()
+        return results
+    elif command == 'headers':
+        cursor.execute("SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_NAME = '{0}';".format(table))
+        results = cursor.fetchall()
+        list = results
+        appendable_headers = []
+        for header in list:
+            appendable_headers.append(header[0])
+        return appendable_headers
+
+
+def get_all_data(cursor, table):
+    records = selector(cursor, 'select_all', table)
+    df = pd.DataFrame(records)
+    headers = selector(cursor, 'headers', table)
+    df.columns = headers
+    df.set_index('id', inplace=True)
+    return df
+
+
+def active_listings(cursor, account):
+    if account != 'all':
+        cursor.execute("SELECT * FROM equipment_listings where account_id = {0} and quantity > 0;".format(account))
+        records = cursor.fetchall()
+        df = pd.DataFrame(records)
+        try:
+            headers = selector(cursor, 'headers', 'equipment_listings')
+            df.columns = headers
+            print(len(df))
+            return df
+        except ValueError:
+            print('No records')
+    else:
+        cursor.execute("SELECT * FROM equipment_listings where quantity > 0;")
+        records = cursor.fetchall()
+        df = pd.DataFrame(records)
+        try:
+            headers = selector(cursor, 'headers', 'equipment_listings')
+            df.columns = headers
+            print(len(df))
+            return df
+        except ValueError:
+            print('No records')
